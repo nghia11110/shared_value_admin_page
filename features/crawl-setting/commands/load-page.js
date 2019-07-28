cdd32dc5fc0b4e45252138c64e29f1f5e97c6c3a
nghiant <nghianguyen248@gmail.com>
2019-07-28 10:33:21 +0000

    finish search by hotelname in crawl-settings page

--- features/crawl-setting/commands/load-page.js
+++ features/crawl-setting/commands/load-page.js
@@ -5,7 +5,8 @@ const { FETCH_INFO_ERROR_MESSAGE } = require('../constants');
 async function loadPageCrawlSettings(req, res) {
   const perPage = 10;
   const page = req.params.page || 1;
-  const condition = { page, perPage};
+  const searchHotelName = req.query.search_hotel_name ? req.query.search_hotel_name : '';
+  const condition = { page, perPage, searchHotelName};
   let data = {};
   try {
     data = await getAllCrawlSettings(condition);
@@ -13,7 +14,7 @@ async function loadPageCrawlSettings(req, res) {
     console.log(error);
     req.session.messages = { errors: { databaseError: FETCH_INFO_ERROR_MESSAGE  } }; 
   }
-  res.render('pages/crawl-settings', { 'crawlSettings': data.data, 'current': data.current_page, 'pages': Math.ceil(data.total / data.per_page), 'per_page': data.per_page });
+  res.render('pages/crawl-settings', { 'crawlSettings': data.data, 'current': data.current_page, 'pages': Math.ceil(data.total / data.per_page), 'per_page': data.per_page, searchHotelName });
 }
 
 module.exports = loadPageCrawlSettings;
--- features/crawl-setting/repository.js
+++ features/crawl-setting/repository.js
@@ -1,7 +1,7 @@
 const knex = require('../../db');
 
 async function getAllCrawlSettings(condition) {
-  const { page, perPage } = condition;
+  const { page, perPage, searchHotelName } = condition;
   const data = await knex('crawl_hotels')
     .leftJoin(knex('crawl_conditions')
       .whereNull('crawl_conditions.deleted_at')
@@ -9,19 +9,27 @@ async function getAllCrawlSettings(condition) {
       this.on('crawl_hotels.hotel_id', '=', 'cc.hotel_id')
         .andOn ('crawl_hotels.site_id', '=', 'cc.site_id')
     })
-    .join('hotels', 'crawl_hotels.hotel_id', '=', 'hotels.id')
+    .join(knex('hotels')
+      .modify(function(queryBuilder) {
+        if (searchHotelName) {
+          queryBuilder.where('name', 'like', '%' + searchHotelName + '%');
+        }
+      })
+      .as('h'), function() {
+        this.on('crawl_hotels.hotel_id', '=', 'h.id')
+    })
     .join('sites', 'crawl_hotels.site_id', '=', 'sites.id')
     .select('crawl_hotels.hotel_id',
       'crawl_hotels.site_id',
       'crawl_hotels.base_url',
-      'hotels.name as hotel_name',
+      'h.name as hotel_name',
       'sites.name as site_name',
       'crawl_hotels.updated_at',
       knex.raw("array_agg(json_build_object('data',cc.*)) as crawl_conditions"))
     .groupBy('crawl_hotels.hotel_id',
       'crawl_hotels.site_id',
       'crawl_hotels.base_url',
-      'hotels.name',
+      'h.name',
       'sites.name',
       'crawl_hotels.updated_at',)
     .whereNull('crawl_hotels.deleted_at')
